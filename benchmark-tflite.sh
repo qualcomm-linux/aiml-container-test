fbefb94b8edaee34738853538b409db670f03ac1
Koen Kooi <koen.kooi@oss.qualcomm.com>
2025-07-08 16:30:06 +0200

    benchmark-tflite.sh: force 'performance' CPUfreq governor
    Signed-off-by: Koen Kooi <koen.kooi@oss.qualcomm.com>

--- benchmark-tflite.sh
+++ benchmark-tflite.sh
@@ -3,9 +3,11 @@
 # Add symlink for libOpenCL, tflite hardcodes the .so, it doesn't properly dynamically link to .so.X
 ln -sf /usr/lib/aarch64-linux-gnu/libOpenCL.so.1 /usr/lib/aarch64-linux-gnu/libOpenCL.so
 
+# For CPUFreq to use performance governer
+for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor ; do echo 'performance' > $i ; done
+
 # This expects models to be present, but we can't distribute them inside the container, so bind mount them using e.g.
 # docker run --volume /path/to/local/models:/root/models <..>
-
 
 cd /root/tensorflow/lite/tools/benchmark/
 
