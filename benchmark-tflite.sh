0e5d70c508383387b1dcf38218adcb44fced814f
Koen Kooi <koen.kooi@oss.qualcomm.com>
2025-07-09 15:43:15 +0200

    benchmark-tflite.sh: print instructions for CPUfreq and hangcheck
    Signed-off-by: Koen Kooi <koen.kooi@oss.qualcomm.com>

--- benchmark-tflite.sh
+++ benchmark-tflite.sh
@@ -3,8 +3,19 @@
 # Add symlink for libOpenCL, tflite hardcodes the .so, it doesn't properly dynamically link to .so.X
 ln -sf /usr/lib/aarch64-linux-gnu/libOpenCL.so.1 /usr/lib/aarch64-linux-gnu/libOpenCL.so
 
-# For CPUFreq to use performance governer
-for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor ; do echo 'performance' > $i ; done
+# For CPUFreq to use performance governer, run outside the container
+echo "Run the following outside the container to have the CPUs run at full tilt:"
+echo 'for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor ; do echo "performance" > $i ; done'
+
+echo
+
+# GPU hangcheck, also run this outside the container
+echo "Extend the GPU hangcheck timer to avoid some models timing out:"
+echo 'echo 6000 > /sys/kernel/debug/dri/0/hangcheck_period_ms'
+
+echo
+echo "Pausing for 30 seconds so you can do the above"
+sleep 10
 
 # This expects models to be present, but we can't distribute them inside the container, so bind mount them using e.g.
 # docker run --volume /path/to/local/models:/root/models <..>
