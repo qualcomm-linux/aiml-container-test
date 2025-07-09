b226fede7a785ca0b375da16f19ad278f457f6d3
Koen Kooi <koen.kooi@oss.qualcomm.com>
2025-07-09 13:24:55 +0200

    benchmark-tflite.sh: use all availabe CPU cores
    Signed-off-by: Koen Kooi <koen.kooi@oss.qualcomm.com>

--- benchmark-tflite.sh
+++ benchmark-tflite.sh
@@ -13,10 +13,10 @@ cd /root/tensorflow/lite/tools/benchmark/
 
 set -x
 for model in $(find /root/models -name "*.tflite") ; do
-#	./benchmark_model --graph=${model} --enable_op_profiling=true --use_xnnpack=true --num_threads=4 --max_sec=300 --profiling_output_csv_file=${model}-gpu.csv  --use_gpu=true |& tee ${model}-gpu-log.txt
-#	./benchmark_model --graph=${model} --enable_op_profiling=true --use_xnnpack=true --num_threads=4 --max_sec=300 --profiling_output_csv_file=${model}-cpu.csv  --use_gpu=false |& tee ${model}-cpu-log.txt
-	./benchmark_model --graph=${model} --use_gpu=true |& tee ${model}-gpu-log.txt
+#	./benchmark_model --graph=${model} --enable_op_profiling=true --use_xnnpack=true --num_threads=$(nproc) --max_sec=300 --profiling_output_csv_file=${model}-gpu.csv  --use_gpu=true |& tee ${model}-gpu-log.txt
+#	./benchmark_model --graph=${model} --enable_op_profiling=true --use_xnnpack=true --num_threads=$(nproc) --max_sec=300 --profiling_output_csv_file=${model}-cpu.csv  --use_gpu=false |& tee ${model}-cpu-log.txt
+	./benchmark_model --graph=${model} --num_threads=$(nproc) --use_gpu=true |& tee ${model}-gpu-log.txt
 	echo "Exit code: " $? >> ${model}-gpu-log.txt
-	./benchmark_model --graph=${model} --use_gpu=false |& tee ${model}-cpu-log.txt
+	./benchmark_model --graph=${model} --num_threads=$(nproc) --use_gpu=false |& tee ${model}-cpu-log.txt
 	echo "Exit code: " $? >> ${model}-cpu-log.txt
 done
