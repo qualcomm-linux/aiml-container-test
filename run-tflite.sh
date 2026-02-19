bcb461c6135d05218276263847b712d0c42daa2f
Koen Kooi <koen.kooi@oss.qualcomm.com>
2026-02-19 11:20:35 +0100

    run-tflite.sh: only run GPU and DSP tests when device nodes are present
    This test needs to run on multiple platforms, where not all accelerates
    will be present, e.g. Agatti has no cdsp. Furthermore, this runs inside
    containers where the device nodes might not passed in by a CDI, on
    purpose or not.
    
    Signed-off-by: Koen Kooi <koen.kooi@oss.qualcomm.com>

--- run-tflite.sh
+++ run-tflite.sh
@@ -12,12 +12,19 @@ set -x
 echo "Running label_image using CPU"
 ./label_image --image=grace_hopper.bmp --use_gpu=false
 
-echo "Running label_image using GPU"
-./label_image --image=grace_hopper.bmp --use_gpu=true
+# Only run when there's at least one device node present
+rendernode="$(ls -1 /dev/card/renderD* | head -n1)"
+if [ -e "${rendernode}" ] ; then
+	echo "Running label_image using GPU"
+	./label_image --image=grace_hopper.bmp --use_gpu=true
+fi
 
-echo "Running label_image using CDSP"
-./label_image --image=grace_hopper.bmp --external_delegate_path=/usr/local/lib/libQnnTFLiteDelegate.so --external_delegate_options='backend_type:htp'
-
+# Only run when there's at least one device node present
+fastrpcnode="$(ls -1 /dev/fastrpc-cdsp* | head -n1)"
+if [ -e "${fastrpcnode}" ] ; then
+	echo "Running label_image using CDSP"
+	./label_image --image=grace_hopper.bmp --external_delegate_path=/usr/local/lib/libQnnTFLiteDelegate.so --external_delegate_options='backend_type:htp'
+fi
 
 cd ~/tensorflow/lite/tools/benchmark
 cp ../../examples/label_image/*mobile* .
@@ -25,8 +32,16 @@ cp ../../examples/label_image/*mobile* .
 echo "running benchmark_model using CPU"
 ./benchmark_model --graph=mobilenet_quant_v1_224.tflite --use_gpu=false
 
-echo "running benchmark_model using GPU"
-./benchmark_model --graph=mobilenet_quant_v1_224.tflite --use_gpu=true
+# Only run when there's at least one device node present
+rendernode="$(ls -1 /dev/card/renderD* | head -n1)"
+if [ -e "${rendernode}" ] ; then
+	echo "running benchmark_model using GPU"
+	./benchmark_model --graph=mobilenet_quant_v1_224.tflite --use_gpu=true
+fi
 
-echo "running benchmark_model using CDSP"
-./benchmark_model --graph=mobilenet_quant_v1_224.tflite --external_delegate_path=/usr/local/lib/libQnnTFLiteDelegate.so --external_delegate_options='backend_type:htp'
+# Only run when there's at least one device node present
+fastrpcnode="$(ls -1 /dev/fastrpc-cdsp* | head -n1)"
+if [ -e "${fastrpcnode}" ] ; then
+	echo "running benchmark_model using CDSP"
+	./benchmark_model --graph=mobilenet_quant_v1_224.tflite --external_delegate_path=/usr/local/lib/libQnnTFLiteDelegate.so --external_delegate_options='backend_type:htp'
+fi
